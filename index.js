3734ef6a0809db291cec67fe6366161ebc062e90
Aayushmaan Yadav <ayush244yadav@gmail.com>
2024-05-06 17:00:47 +0530

    Learnt get and post using postman api

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const users=require('./MOCK_DATA.json');
-
+const fs=require('fs');
 
 const app = express();
 const port = 3000;
+
+//Middleware-Plugin
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -21,15 +24,64 @@ app.get('/users',(req,res)=>{
 app.get('/api/users',(req,res)=>{
     return res.json(users);
 })
-
-app.get('/users/:id',(req,res)=>{
+app.get('/api/users/:id',(req,res)=>{
     const id=Number(req.params.id);
-    const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
-        user.id==id
-    );
-    return res.json(user);
+        const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
+            user.id==id
+        );
+        return res.json(user);
 })
 
+app.route("/users/:id")
+    .get((req,res)=>{
+        const id=Number(req.params.id);
+        const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
+            user.id==id
+        );
+        return res.json(user);
+    })
+    
+    .patch((req,res)=>{
+        //Edit user with id here
+        res.json({status:"pending"})
+    })
+    .delete((req,res)=>{
+        //delete user with id here
+        res.json({status:"pending"})
+    })
+app.post("/api/users",(req,res)=>{
+    const body=req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"Success",id:users.length+1});
+    });
+})
+
+//This makes it easy to handle rather than the method written below
+
+// app.get('/users/:id',(req,res)=>{
+//     const id=Number(req.params.id);
+//     const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
+//         user.id==id
+//     );
+//     return res.json(user);
+// })
+
+
+// app.post("/api/users",(req,res)=>{
+//     //Create new user here
+//     res.json({status:"pending"})
+// })
+
+// app.patch("/api/users/:id",(req,res)=>{
+//     //Edit user with id here
+//     res.json({status:"pending"})
+// })
+
+// app.delete("/api/users/:id",(req,res)=>{
+//     //delete user with id here
+//     res.json({status:"pending"})
+// })
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
