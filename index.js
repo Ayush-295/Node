b10fb20c34e5608a2f552740b46fcc787e665aaf
Aayushmaan Yadav <ayush244yadav@gmail.com>
2024-05-08 13:47:40 +0530

    Middlewares

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
-const express=require('express');
-const users=require('./MOCK_DATA.json');
-const fs=require('fs');
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-//Middleware-Plugin
-app.use(express.urlencoded({extended:false}))
+// Middleware-Plugin
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use((req, res, next) => {
+  req.myUserName = "Aayushmaan";
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}:${req.method}:${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+  console.log("hello from middleware 1");
+  next();
 });
 
-app.get('/users',(req,res)=>{
-    const html=`
-    <ul>${users.map((user)=>
-        `<li>${user.first_name}</li>`
-    ).join("")}</ul>
-    `
-    res.send(html);
-})
+app.get("/", (req, res) => {
+  res.send(`Hello ${req.myUserName}!`);
+});
 
-app.get('/api/users',(req,res)=>{
-    return res.json(users);
-})
-app.get('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id);
-        const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
-            user.id==id
-        );
-        return res.json(user);
-})
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>${users.map((user) => `<li>${user.first_name}</li>`).join("")}</ul>
+    `;
+  res.send(html);
+});
 
-app.route("/users/:id")
-    .get((req,res)=>{
-        const id=Number(req.params.id);
-        const user=users.find((user)=>     //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
-            user.id==id
-        );
-        return res.json(user);
-    })
-    
-    .patch((req,res)=>{
-        //Edit user with id here
-        res.json({status:"pending"})
-    })
-    .delete((req,res)=>{
-        //delete user with id here
-        res.json({status:"pending"})
-    })
-app.post("/api/users",(req,res)=>{
-    const body=req.body;
-    users.push({...body,id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        return res.json({status:"Success",id:users.length+1});
-    });
-})
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find(
+    (
+      user //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
+    ) => user.id == id
+  );
+  return res.json(user);
+});
+
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(
+      (
+        user //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
+      ) => user.id == id
+    );
+    return res.json(user);
+  })
+
+  .patch((req, res) => {
+    //Edit user with id here
+    res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    //delete user with id here
+    res.json({ status: "pending" });
+  });
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length + 1 });
+  });
+});
 
 //This makes it easy to handle rather than the method written below
 
@@ -66,7 +82,6 @@ app.post("/api/users",(req,res)=>{
 //     );
 //     return res.json(user);
 // })
-
 
 // app.post("/api/users",(req,res)=>{
 //     //Create new user here
@@ -83,7 +98,6 @@ app.post("/api/users",(req,res)=>{
 //     res.json({status:"pending"})
 // })
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
-
