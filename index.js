fc7a15dc01d74b0119d917e08900fa2a1b021f28
Aayushmaan Yadav <ayush244yadav@gmail.com>
2024-05-09 19:48:11 +0530

    Made schema and crud operations using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 const fs = require("fs");
+const { type } = require("os");
+
+//Connection with mongoDB
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongo-app-1")
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("Mongo Error", err);
+  });
+
+//Schema
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//Model
+
+const User = mongoose.model("User", userSchema);
 
 const app = express();
 const port = 3000;
@@ -26,56 +68,59 @@ app.get("/", (req, res) => {
   res.send(`Hello ${req.myUserName}!`);
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const allDbusers = await User.find({});
   res.setHeader("X-MyName", "Aayushmaan"); //Use X-{HeaderName} to set custom headers .
   const html = `
-    <ul>${users.map((user) => `<li>${user.first_name}</li>`).join("")}</ul>
+    <ul>${allDbusers
+      .map((user) => `<li>${user.firstName}-${user.email}</li>`)
+      .join("")}</ul>
     `;
   res.send(html);
 });
 
-app.get("/api/users", (req, res) => {
-  return res.json(users);
+app.get("/api/users", async (req, res) => {
+  const allDbusers = await User.find({});
+  return res.json(allDbusers);
 });
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find(
-    (
-      user //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
-    ) => user.id == id
-  );
+app.get("/api/users/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
   return res.json(user);
 });
 
 app
   .route("/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find(
-      (
-        user //When a function doesnt return anything it has to be without curly braces else the webpage doesnt get loaded
-      ) => user.id == id
-    );
+  .get(async (req, res) => {
+    const user = await User.findById(req.params.id);
     return res.json(user);
   })
 
-  .patch((req, res) => {
+  .patch(async (req, res) => {
     //Edit user with id here
-    res.json({ status: "pending" });
+    await User.findByIdAndUpdate(req.params.id, { firstName: "changed" });
+    return res.json({ status: "Success" });
   })
-  .delete((req, res) => {
+  .delete(async(req, res) => {
+    await User.findByIdAndDelete(req.params.id);
     //delete user with id here
-    res.json({ status: "pending" });
+    return res.json({ status: "Success" });
   });
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
   if (!body || !body.first_name || !body.email || !body.gender) {
     return res.status(400).send("There is an error in your data");
+  } else {
+    await User.create({
+      firstName: body.first_name,
+      lastname: body.last_name,
+      email: body.email,
+      gender: body.gender,
+      job_title: body.job_title,
+    });
+    return res.status(201).send("User created");
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "Success", id: users.length + 1 });
-  });
+
+  // console.log(res);
 });
 
 //This makes it easy to handle rather than the method written below
